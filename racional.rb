88c333993c0f1ad631d514188aa5b5fab4245850
Victor Plaza <M4tus3l4@hotmail.com>
2013-10-18 18:52:21 +0100

    Operaciones finalizadas con ejemplos

--- racional.rb
+++ racional.rb
@@ -34,7 +34,9 @@ class Fraccion
        segundo = Fraccion.new((mcm/fraccion.den) * fraccion.num, mcm)
        minexpresion(Fraccion.new(primero.num + segundo.num, mcm))
     end
-
+    
+    
+    #Método para realizar una resta y minimizar el resultado.
     def resta(fraccion)
        mcm = (@den * fraccion.den)/gcd(@den, fraccion.den)
        primero = Fraccion.new((mcm/@den) * @num, mcm)
@@ -42,22 +44,41 @@ class Fraccion
        minexpresion(Fraccion.new(primero.num - segundo.num, mcm))
     end
 
+    
+    #Método que realiza el producto entre dos fracciones y minimiza el resultado
     def producto(fraccion)
        minexpresion(Fraccion.new(@num * fraccion.num, @den * fraccion.den))
     end
 
+    
+    #Método que realiza la división entre dos fracciones y minimiza el resultado
     def division(fraccion)
        minexpresion(Fraccion.new(@num * fraccion.den, @den * fraccion.num))
     end
 
 end
 
-
+#Ejecución de algunas operaciones.
 a=Fraccion.new(13,4)
 b=Fraccion.new(7,5)
 
+puts "La fracción 'a' es:"
 a.to_s
+puts "La fracción 'b' es:"
 b.to_s
 
 c = a.suma(b)
+puts "\nLa suma de a + b es:"
+c.to_s
+
+c = a.resta(b)
+puts "\nLa resta de a - b es:"
+c.to_s
+
+c = a.producto(b)
+puts "\nEl producto de a * b es:"
+c.to_s
+
+c = a.division(b)
+puts "\nLa división de a / b es:"
 c.to_s
